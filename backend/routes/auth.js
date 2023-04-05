740aa4255a1c3fa2e8dacdf3b5706cfaabc3d5ff
Ishvinder Singh <ishvinder133@gmail.com>
2023-04-06 00:50:27 +0530

    create post and get request

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,22 +2,50 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const Notes = require("../models/Notes");
+const { body, validationResult } = require("express-validator");
 
 // post the data to the /api/auth
-router.post("/", (req, res) => {
+router.post(
+  "/",
+  [
+    body("name", "invalid name, enter more than 5 character").isLength({
+      min: 5,
+    }),
+    body("email", "invalid email").isEmail(),
+    body("password", "invalid name, enter more than 5 character").isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {
+    const { name, email, password } = req.body;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      const userAdded = await User.create({
+        name: name,
+        email: email,
+        password: password,
+      });
+      console.log(req.body);
+      res.status(200).json(userAdded);
+    } catch (error) {
+      console.log("Error Found => ", error);
+      res.status(400).json({ Error: error.message });
+    }
+  }
+);
+
+//  Get data
+router.get("/", async (req, res) => {
   try {
-    const user = User(req.body);
-    console.log(req.body);
-    user.save();
-    res.send(user);
+    const showAll = await User.find();
+    res.status(200).json(showAll);
   } catch (error) {
-    console.log("Error Found => ", error.message);
+    console.log("Error Found => ", error);
+    res.status(400).json({ error: error.message });
   }
 });
-
-// router.get('/', async(req, res) => {
-//    const response = await fetch()
-//     res.send()
-// })
 
 module.exports = router;
